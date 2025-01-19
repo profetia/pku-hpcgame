82855f0104cb15f15f6dfd0e056b82a09dee0280
yanglsh <yanglsh@shanghaitech.edu.cn>
2025-01-19 10:19:24 +0000

    feat(c): rewrite in Fortran

--- c/filter.F90
+++ c/filter.F90
@@ -1,15 +1,31 @@
-void filter_run(double x[113][2700], double wgt[113][1799], int ngrid[113], int is, int ie, int js, int je)
-{
-    double tmp[ie - is + 1];
-    for(int j = js; j <= je; j++){
-        int n = ngrid[j];
-        int hn = (n - 1) >> 1;
-        for(int i = is; i <= ie; i++){
-            tmp[i - is] = 0;
-            for(int p = 0; p < n; p++)
-                tmp[i - is] += wgt[j][p] * x[j][i - hn + p];
-        }
-        for(int i = is; i <= ie; i++)
-            x[j][i] = tmp[i - is];
-    }
-}
+
+subroutine filter_run(x, wgt, ngrid, is, ie, js, je) bind(C, name="filter_run")
+    implicit none
+    real(8) :: x(2700, 113)
+    real(8) :: wgt(1799, 113)
+    integer(4) :: ngrid(113)
+    integer(4), value :: is, ie, js, je
+
+    integer(4) :: is_f, ie_f, js_f, je_f
+    real(8) :: tmp(ie - is + 1)
+    integer(4) :: i, j, p, n, hn
+    
+    is_f = is + 1
+    ie_f = ie + 1
+    js_f = js + 1
+    je_f = je + 1
+
+    do j = js_f, je_f
+        n = ngrid(j)
+        hn = (n - 1) / 2
+        do i = is_f, ie_f
+            tmp(i - is_f + 1) = 0
+            do p = 1, n
+                tmp(i - is_f + 1) = tmp(i - is_f + 1) + wgt(p, j) * x(i - hn + p - 1, j)
+            end do
+        end do
+        do i = is_f, ie_f
+            x(i, j) = tmp(i - is_f + 1)
+        end do
+    end do
+end subroutine filter_run
