0ffccc202ade07f9ffa378621a900e7e0d7b6148
yanglsh <yanglsh@shanghaitech.edu.cn>
2025-01-20 01:28:08 +0000

    feat(d): waterfront

--- d/lcs.cpp
+++ d/lcs.cpp
@@ -1,28 +1,50 @@
-#include <utility>
-#include <cstdlib>
 #include <algorithm>
+#include <cstdlib>
+#include <utility>
 
 typedef int element_t;
 
 size_t lcs(element_t* arr_1, element_t* arr_2, size_t len_1, size_t len_2) {
-    size_t* previous = (size_t*)calloc(len_2 + 1, sizeof(size_t));
-    size_t* current = (size_t*)calloc(len_2 + 1, sizeof(size_t));
+  size_t* mm[3];
+  mm[0] = (size_t*)calloc(len_1 + 1, sizeof(size_t));
+  mm[1] = (size_t*)calloc(len_1 + 1, sizeof(size_t));
+  mm[2] = (size_t*)calloc(len_1 + 1, sizeof(size_t));
 
-    for (size_t i = 1; i <= len_1; ++i) {
-        for (size_t j = 1; j <= len_2; ++j) {
-            if (arr_1[i - 1] == arr_2[j - 1]) {
-                current[j] = previous[j - 1] + 1;
-            } else {
-                current[j] = std::max(current[j - 1], previous[j]);
-            }
-        }
-        std::swap(current, previous);
+  size_t diag, mm_index;
+  size_t i, j, i_start, i_end;
+  size_t *mm_this, *mm_minus_1, *mm_minus_2;
+#pragma omp parallel default(none)                                             \
+    shared(mm, arr_1, arr_2, len_1, len_2) private(diag, mm_index, mm_this,    \
+                                                       mm_minus_1, mm_minus_2, \
+                                                       i, j, i_start, i_end)
+  for (diag = 2, mm_index = 2; diag <= len_1 + len_2;
+       ++diag, mm_index = (mm_index + 1) % 3) {
+    mm_this = mm[mm_index];
+    mm_minus_1 = mm[(mm_index + 2) % 3];
+    mm_minus_2 = mm[(mm_index + 1) % 3];
+
+    i_start = diag <= len_2 ? 1 : diag - len_2;
+    i_end = std::min(len_1, diag - 1);
+#pragma omp parallel for
+    // #pragma omp unroll partial(16)
+    for (i = i_start; i <= i_end; ++i) {
+      j = diag - i;
+
+      if (arr_1[i - 1] == arr_2[j - 1]) {
+        mm_this[i] = mm_minus_2[i - 1] + 1;
+      } else {
+        mm_this[i] = std::max(mm_minus_1[i], mm_minus_1[i - 1]);
+      }
     }
 
-    size_t result = previous[len_2];
+#pragma omp barrier
+  }
 
-    free(previous);
-    free(current);
+  size_t result = mm[mm_index][len_1];
 
-    return result;
+  free(mm[0]);
+  free(mm[1]);
+  free(mm[2]);
+
+  return result;
 }
--- d/scripts/check.sh
+++ d/scripts/check.sh
@@ -3,16 +3,24 @@
 data_dir="data"
 app="./lcs"
 
-for i in {0..4}; do
-    echo "Checking case $i"
-
+function check_impl {
+    echo "Checking case $1"
     rm -f input.dat output.txt
-    cp $data_dir/case$i.in input.dat
+    cp $data_dir/case$1.in input.dat
     $app
-    diff -q $data_dir/case$i.out output.txt
+    diff -q $data_dir/case$1.out output.txt
     if [ $? -eq 0 ]; then
-        echo "$i: Ok"
+        echo "$1: Ok"
     else
-        echo "$i: Fail"
+        echo "$1: Fail"
     fi
+}
+
+if [ $# -eq 1 ]; then
+    check_impl $1
+    exit
+fi
+
+for i in {0..4}; do
+    check_impl $i
 done
