ef685ec24674b996b9abb10c926b0e11eab1ccd0
yanglsh <yanglsh@shanghaitech.edu.cn>
2025-01-21 07:23:12 -0700

    feat(g): parallel with 8 bits radix

--- g/topk/impl.jl
+++ g/topk/impl.jl
@@ -1,3 +1,5 @@
+using Base.Threads
+
 # 原子类型定义，用于线程安全的操作
 mutable struct Atomic{T}
     @atomic x::T
@@ -8,9 +10,11 @@ const Bitwise = UInt64
 const Index = Int32
 const Count = Int32
 
-const RADIX_BITS::Index = 2
+const RADIX_BITS::Index = 8
 const RADIX_SIZE::Index = 1 << RADIX_BITS
 const RADIX_MASK::Bitwise = RADIX_SIZE - 1
+
+const NUM_THREADS::Index = 8
 
 @inbounds function convert(value::Int64)::Scalar
     return Scalar(9223372036854775808) + value
@@ -45,16 +49,16 @@ end
     return (data & ~mask) | insert
 end
 
-@inbounds function count_radix_using_mask(
+@inbounds function count_radix_using_mask!(
     data::AbstractVector{T},
     desired::Bitwise,
     mask::Bitwise,
     location::Index,
-)::Array{Count} where {T}
+    counts::Vector{Count},
+) where {T}
 
-
-    counts::Array{Count} = zeros(Count, RADIX_SIZE)
-    for value in data
+    counts_local::Vector{Count} = zeros(Count, RADIX_SIZE * NUM_THREADS)
+    @threads for value in data
         scalar::Scalar = convert(value)
 
         is_valid::Bool = (scalar & mask) == desired
@@ -63,27 +67,36 @@ end
         end
 
         radix::Index = get_bit_field(scalar, location, RADIX_BITS)
-        counts[radix+1] += 1
+        thread_index::Index = threadid()
+        counts_local[radix+(thread_index-1)*RADIX_SIZE+1] += one(Count)
     end
 
-    return counts
+    counts .= zero(Count)
+    @simd for offset in 0:NUM_THREADS-1
+        @simd for i in eachindex(counts)
+            counts[i] += counts_local[i+offset*RADIX_SIZE]
+        end
+    end
 end
 
 @inbounds function find_pattern(
     data::AbstractVector{T},
     k::Index,
     desired::Bitwise,
-)::Array{Index} where {T}
+)::Vector{Index} where {T}
 
-    topk::Array{Index} = zeros(Index, k)
-    topk_count::Index = 1
+    topk::Vector{Index} = Vector{Index}(undef, k)
+    topk_count::Atomic{Index} = Atomic{Index}(one(Index))
 
-    for (index, value) in enumerate(data)
-        scalar::Scalar = convert(value)
+    @threads for i in eachindex(data)
+        scalar::Scalar = convert(data[i])
 
         if scalar >= desired
-            topk[topk_count] = index
-            topk_count += 1
+            topk_index::Index = (
+                @atomic topk_count.x += one(Index)
+            ) - 1
+
+            topk[topk_index] = i
         end
     end
 
@@ -93,22 +106,23 @@ end
 @inbounds function radix_select(
     data::AbstractVector{T},
     k::Index,
-)::Array{Index} where {T}
+)::Vector{Index} where {T}
 
-    desired::Bitwise = 0
-    mask::Bitwise = 0
+    desired::Bitwise = zero(Bitwise)
+    mask::Bitwise = zero(Bitwise)
 
     k_remaining::Index = k
+    counts::Vector{Count} = Vector{Count}(undef, RADIX_SIZE)
     for location::Index in
         (sizeof(Scalar)*8-RADIX_BITS):-RADIX_BITS:0
 
-        counts::Array{Count} = count_radix_using_mask(
-            data, desired, mask, location)
+        count_radix_using_mask!(
+            data, desired, mask, location, counts)
 
         for radix::Index in RADIX_SIZE-1:-1:0
             count::Count = counts[radix+1]
 
-            if count == 1 && k_remaining == 1
+            if count == one(Count) && k_remaining == one(Index)
                 desired = set_bit_field(desired, Bitwise(radix), location, RADIX_BITS)
                 mask = set_bit_field(mask, RADIX_MASK, location, RADIX_BITS)
                 return find_pattern(data, k, desired)
@@ -128,7 +142,7 @@ end
 end
 
 @inbounds function topk(data::AbstractVector{T}, k) where {T}
-    result::Array{Index} = radix_select(data, Index(k))
+    result::Vector{Index} = radix_select(data, Index(k))
     return sort(result;
         lt=(x, y) ->
             data[x] > data[y] || (data[x] == data[y] && x < y))
--- g/topk/test.jl
+++ g/topk/test.jl
@@ -13,11 +13,11 @@ f64_1m = randn(Float64, 1 << 20)
 # 1M
 println("Testing on 1M data...")
 res, time_i64_1m = @timed topk(i64_1m, 1 << 8)
-ref, time_i64_1m_base = @timed partialsortperm(i64_1m, 1:1 << 8, rev=true)
+ref, time_i64_1m_base = @timed partialsortperm(i64_1m, 1:1<<8, rev=true)
 @assert all(res .== ref) "Failed for Int64 1M"
 
 res, time_f64_1m = @timed topk(f64_1m, 1 << 8)
-ref, time_f64_1m_base = @timed partialsortperm(f64_1m, 1:1 << 8, rev=true)
+ref, time_f64_1m_base = @timed partialsortperm(f64_1m, 1:1<<8, rev=true)
 @assert all(res .== ref) "Failed for Float64 1M"
 
 println("Base: ", [time_i64_1m_base, time_f64_1m_base])
@@ -30,11 +30,11 @@ println("Speedup: ", [time_i64_1m_base / time_i64_1m, time_f64_1m_base / time_f6
 
 # println("Testing on 1B data...")
 # res, time_i64_1b = @timed topk(i64_1b, 1 << 10)
-# ref, time_i64_1b_base = @timed partialsortperm(i64_1b, 1:1 << 10, rev=true)
+# ref, time_i64_1b_base = @timed partialsortperm(i64_1b, 1:1<<10, rev=true)
 # @assert all(res .== ref) "Failed for Int64 1B"
 
 # res, time_f64_1b = @timed topk(f64_1b, 1 << 10)
-# ref, time_f64_1b_base = @timed partialsortperm(f64_1b, 1:1 << 10, rev=true)
+# ref, time_f64_1b_base = @timed partialsortperm(f64_1b, 1:1<<10, rev=true)
 # @assert all(res .== ref) "Failed for Float64 1B"
 
 # println("Base: ", [time_i64_1b_base, time_f64_1b_base])
